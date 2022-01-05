a6ed6e3310cdc61d78d7901519ead2e3b66762e6
Miguel Lamm <49272909+MiguelLamm@users.noreply.github.com>
2022-01-05 15:08:13 +0100

    bugfix
    time would not show because of if error

--- day4_TileGame/script/mem.js
+++ day4_TileGame/script/mem.js
@@ -3,13 +3,7 @@ let mi = window.localStorage.getItem('minutes');
 let se = window.localStorage.getItem('seconds');
 let hu = window.localStorage.getItem('hundreds');
 document.getElementById('resultscore').innerHTML = `Your score: ${fi} points`;
-if (fi <= 0) {
-    document.getElementById('msg').innerHTML = 'Did you do it on purpose or do you just fail at everything?'
-} else if (fi > 200) {
-    document.getElementById('msg').innerHTML = 'AMAZING PERFORMANCE!'
-} else if (0 <= fi <= 200) {
-    document.getElementById('msg').innerHTML = 'Doing good, keep going!'
-}
+
 if(se<10){
     document.getElementById('resulttime').innerHTML = `Your time: 0${mi}:0${se}:${hu}`;
 } else{
